de877249917ba92be9a73d023abd96883cce8efb
jameshrybinczak <hrybinczakj@yahoo.com>
2022-04-06 22:03:58 -0400

    James

--- src/SetUp.java
+++ src/SetUp.java
@@ -1,3 +1,10 @@
 public class SetUp {
     // Setting up project for group 18
+    public static void main(String[] args) {
+
+        String str1 = "James";
+        System.out.println(str1);
+
+
+    }
 }
